b8ac4e66ec20b5cf2552d1554618bf32baa01446
Anyssa Iwamoto <akiwamot@asu.edu>
2019-09-16 15:57:38 -0700

    added all the mockup devices

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# automate
+### automate
 Attempting to automate the task sequence for creating reference guides.
+
+The process for creating reference guides are as follows:
+1. Accumulate necessary screenshots
+2. Generate final graphics by placing the screenshots in the corresponding device mockup (step01_deviceMockup)
+3. Create JSON file with written instructions
+4. Generate reference guide printout with final graphics and written instructions (step02_guidePrintout)
+5. Generate thumbnails for YouTube and Gecko Portal (step03_thumbnails)
+6. Generate video tutorial based on final graphics and written instructions (step04_videoTutorial)
+7. Upload final video to YouTube
+8. Upload guide, video, and instructions to the Gecko Portal website
--- step01_deviceMockup.jsx
+++ step01_deviceMockup.jsx
@@ -5,7 +5,7 @@
 
 // OPEN FILE --------------------------------------------------------------------------------------------------------
 // Load step01_deviceMockup.psd file (change absolute path as necessary)
-const deviceMockup = File("D:/automate/workingFiles/step01_deviceMockup.psd");
+const deviceMockup = File("D:/automationFiles/workingFiles/step01_deviceMockup.psd");
 app.open(deviceMockup);
 
 // SELECTION PANEL --------------------------------------------------------------------------------------------------
@@ -34,25 +34,58 @@ app.open(deviceMockup);
                 deviceList.margins = 10; 
 
             // DEVICE MOCKUPS
-            var device1 = deviceList.add("radiobutton", undefined, undefined, {name: "touchPanel-crestronTSW760B"}); 
+            var device1 = deviceList.add("radiobutton", undefined, undefined, {name: "touchPanel-crestron-tsw760b"}); 
                 device1.text = "Touch Panel - Crestron TSW760B";
 
-            var device2 = deviceList.add("radiobutton", undefined, undefined, {name: "television-viewSonicIFP8650"}); 
+            var device2 = deviceList.add("radiobutton", undefined, undefined, {name: "television-viewSonic-ifp8650"}); 
                 device2.text = "Television - ViewSonic IFP8650"; 
 
-            var device3 = deviceList.add("radiobutton", undefined, undefined, {name: "monitor-acerVA270H"}); 
+            var device3 = deviceList.add("radiobutton", undefined, undefined, {name: "monitor-acer-va270h"}); 
                 device3.text = "Monitor - Acer VA270H"; 
 
-            var device4 = deviceList.add("radiobutton", undefined, undefined, {name: "monitor-lg27MK60TMB"}); 
+            var device4 = deviceList.add("radiobutton", undefined, undefined, {name: "monitor-lg-27mk60tmb"}); 
                 device4.text = "Monitor - LG 27MK60TMB"; 
+
+            var device5 = deviceList.add("radiobutton", undefined, undefined, {name: "monitor-apple-imacpro"}); 
+                device5.text = "Monitor - Apple iMac Pro";
+
+            var device6 = deviceList.add("radiobutton", undefined, undefined, {name: "monitor-apple-imac"}); 
+                device6.text = "Monitor - Apple iMac"; 
+
+            var device7 = deviceList.add("radiobutton", undefined, undefined, {name: "laptop-dell-xps15"}); 
+                device7.text = "Laptop - Dell XPS 15"; 
+
+            var device8 = deviceList.add("radiobutton", undefined, undefined, {name: "laptop-acer-travelmatespinb1"}); 
+                device8.text = "Laptop - Acer TravelMate Spin B1"; 
             
+            var device9 = deviceList.add("radiobutton", undefined, undefined, {name: "laptop-apple-macbookair"}); 
+                device9.text = "Laptop - Apple Macbook Air";
+
+            var device10 = deviceList.add("radiobutton", undefined, undefined, {name: "laptop-dell-precisionmobileworkstation5000s"}); 
+                device10.text = "Laptop - Dell Precision Mobile Workstation 5000s"; 
+
+            var device11 = deviceList.add("radiobutton", undefined, undefined, {name: "phone-oneplus-6"}); 
+                device11.text = "Phone - OnePlus 6"; 
+
+            var device12 = deviceList.add("radiobutton", undefined, undefined, {name: "phone-samsung-s9"}); 
+                device12.text = "Phone - Samsung S9"; 
+
+            var device13 = deviceList.add("radiobutton", undefined, undefined, {name: "phone-samsung-galaxys10e"}); 
+                device13.text = "Phone - Samsung Galaxy S10E";
+
+            var device14 = deviceList.add("radiobutton", undefined, undefined, {name: "phone-essential-ph1"}); 
+                device14.text = "Phone - Essential PH1"; 
+
+            var device15 = deviceList.add("radiobutton", undefined, undefined, {name: "phone-apple-iphone6s"}); 
+                device15.text = "Phone - Apple iPhone 6S"; 
+
             deviceList.children[0].value = true;
 
         var rightColumn = selectionPanel.add("group", undefined, {name: "rightColumn"}); 
             rightColumn.orientation = "column"; 
             rightColumn.alignChildren = ["fill","top"]; 
             rightColumn.spacing = 10; 
-            rightColumn.margins = 0; 
+            rightColumn.margins = 0;
 
         var ok = rightColumn.add("button", undefined, undefined, {name: "ok"}); 
             ok.text = "OK"; 
@@ -69,23 +102,26 @@ app.open(deviceMockup);
         switch(true) {
             case device1.value:
                 // Make device folder visible
-                app.activeDocument.layerSets.getByName("touchPanel-crestronTSW760B").visible = true;
+                app.activeDocument.layerSets.getByName("touchPanel-crestron-tsw760b").visible = true;
                 // Assigning smart object for corresponding layer to variable 'smartObjectLayer'
                 var smartObjectLayer = activeDocument.layerSets[0].artLayers[0];
                 // Should return "crestron-tsw760b-screen"
-                // alert(smartObjectLayer.name)
+                alert(smartObjectLayer.name)
+                // Load step01_deviceMockup.psb file (change absolute path as necessary)
+                // var openSmartObjectFile = File("D:/automationFiles/workingFiles/smartObjectLinks/touchPanel-crestron-tsw760b-screen.psb");
+                // app.open(deviceMockup);
                 break;
 
             case device2.value:
-                app.activeDocument.layerSets.getByName("television-viewSonicIFP8650").visible = true;
+                app.activeDocument.layerSets.getByName("television-viewSonic-ifp8650").visible = true;
                 break;
 
             case device3.value:
-                app.activeDocument.layerSets.getByName("monitor-acerVA270H").visible = true;
+                app.activeDocument.layerSets.getByName("monitor-acer-va270h").visible = true;
                 break;
 
             case device4.value:
-                app.activeDocument.layerSets.getByName("monitor-lg27MK60TMB").visible = true;
+                app.activeDocument.layerSets.getByName("monitor-lg-27mk60tmb").visible = true;
                 break;
         }
 
@@ -94,9 +130,13 @@ app.open(deviceMockup);
             this.parent.close();
         }
 
-// IMPORT IMAGES ----------------------------------------------------------------------------------------------------
+// SMART LAYER REPLACE ----------------------------------------------------------------------------------------------------
 // Based on user selection, the corresponding smart object should:
 //   1) Import one image at a time from the folder selected
 //   2) Resize the image to match the canvas size
-//   3) Export the full mockup
+//   3) Save
+//   4) Switch activeDocument to mockup
+//   5) Export for web into folder selected
 //   4) Repeat the process till all files within the folder have been imported
+
+// openSmartObjectFile = app.activeDocument;
